210fbc42c3e6ccfbf7faf0095b6f57aab368cce2
Dezerae Cox <32660750+dezeraecox@users.noreply.github.com>
2021-05-11 21:23:30 +1000

    Generate predict labels function

--- utils/predict.py
+++ utils/predict.py
@@ -1,5 +1,6 @@
 import os
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 from tensorflow import keras
@@ -14,27 +15,30 @@ if not os.path.exists(output_folder):
 
 def prepare_data_to_predict(time_data):
 
-    if len(time_data.shape) == 2:  # if univariate
-        # add a dimension to make it multivariate with one dimension 
-        time_data = time_data.values.reshape((time_data.shape[0], time_data.shape[1], 1))
+    return time_data.values.reshape((time_data.shape[0], time_data.shape[1], 1))
+
+def predict_labels(time_data, model_path):
+    # evaluate best model on new dataset
+    x_predict = prepare_data_to_predict(time_data)
+    model = keras.models.load_model(model_path)
+    y_pred = model.predict(x_predict)
+    y_pred = np.argmax(y_pred, axis=1)
+
+    # Add labels back to original dataframe
+    time_data['label'] = y_pred
 
     return time_data
 
-# Read in dataset
-raw_data = pd.read_csv(data_path)
-raw_data.drop([col for col in raw_data.columns.tolist() if 'Unnamed: ' in col], axis=1, inplace=True)
+if __name__ == '__main__':
 
-# prepare time series data
-time_data = raw_data[[col for col in raw_data.columns.tolist() if col not in ['molecule_number', 'label']]].T.reset_index(drop=True)
+    # Read in dataset
+    raw_data = pd.read_csv(data_path)
+    raw_data.drop([col for col in raw_data.columns.tolist() if 'Unnamed: ' in col], axis=1, inplace=True)
 
-# evaluate best model on new dataset
-x_predict = prepare_data_to_predict(time_data)
-model = keras.models.load_model(model_path)
-y_pred = model.predict(x_predict)
-y_pred = np.argmax(y_pred, axis=1)
+    # prepare time series data
+    time_data = raw_data[[col for col in raw_data.columns.tolist() if col not in ['molecule_number', 'label']]].T.reset_index(drop=True)
 
-# Add labels back to original dataframe
-raw_data['label'] = y_pred
+    time_data = predict_labels(time_data, model_path)
 
-# Save to csv
-raw_data.to_csv(f'{output_folder}predicted_labels.csv')
+    # Save to csv
+    raw_data.to_csv(f'{output_folder}predicted_labels.csv')
